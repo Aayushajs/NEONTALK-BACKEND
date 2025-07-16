719a708e639be7debae113d1a66204aa6bdbcda3
Aayushajs <aayushj004@gmail.com>
2025-07-17 00:44:44 +0530

    this is NEONTALK app to use team

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -51,11 +51,10 @@ export async function signup(req, res) {
     });
 
     res.cookie("jwt", token, {
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
-      //secure: process.env.NODE_ENV === "production",
-      secure: false,
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+      httpOnly: true, // Set httpOnly to true to prevent XSS attacks
+      sameSite: 'None', // Set sameSite to None to allow third-party cookies
+      secure: true, 
     });
 
     res.status(201).json({ success: true, user: newUser });
@@ -84,11 +83,10 @@ export async function login(req, res) {
     });
 
     res.cookie("jwt", token, {
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
-      //secure: process.env.NODE_ENV === "production",
-      secure: false,
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+      httpOnly: true, // Set httpOnly to true to prevent XSS attacks
+      sameSite: 'None', // Set sameSite to None to allow third-party cookies
+      secure: true, 
     });
 
     res.status(200).json({ success: true, user });
